531ea0d79dd63cb07070e6bd1bc2984f4672330e
cholguin <cholguin@cohan.org.co>
2025-05-03 10:37:01 -0500

    Se crean diferentes funciones
    Se crean diferentes funciones

--- homework/src/wordcount.py
+++ homework/src/wordcount.py
@@ -1,45 +1,33 @@
 # obtain a list of files in the input directory
 import os
 
-# Read all lines from the input files
-def read_all_lines():
-    all_lines = []
-    input_file_list = os.listdir('data/input/')
-    for filename in input_file_list:
-        with open(filename, "r", encoding="utf-8") as f:
-            lines += f.readlines()
-            all_lines.extend(lines)
-    return all_lines
-
+from homework.src._internals import read_all_lines, preprocess_lines, split_in_words, count_words, write_count_words
 
 def main():
 
-    input_file_list = os.listdir('data/input/')
-    #all_lines = read_all_lines()
+     ## mover a la funcion "read_all_lines"
+    all_lines = read_all_lines()
     
+     ## mover a "preprocess_lines"
+    all_lines = preprocess_lines(all_lines)
+
+    ## mover "split_in_words"
+    words = split_in_words(all_lines)
+
+    ## mover a "count_words"
+    counter = count_words(words)
 
     # count the frequency of the words in the files in the input directory
-    counter={}
-    for filename in input_file_list:
-        with open('data/input/'+filename) as f:
-            for l in f:
-                for w in l.split( ):
-                    w = w.lower().strip(",.!?")
-                    counter[w] = counter.get(w, 0) + 1
+    # counter={}
+    # for filename in input_file_list:
+    #     with open('data/input/'+filename) as f:
+    #         for l in f:
+    #             for w in l.split( ):
+    #                 w = w.lower().strip(",.!?")
+    #                 counter[w] = counter.get(w, 0) + 1
 
-    # create the directory output/ if it doesn't exist
+    ##
     write_count_words(counter)
-
-def write_count_words(counter):
-
-    if not os.path.exists('data/output'):
-        os.makedirs('data/output')
-
-    # save the results using tsv format
-    with open("data/output/results.tsv", "w", encoding="utf-8") as f:
-            for key, value in counter.items():
-                # write the key and value to the file
-                f.write(f"{key}\t{value}\n")
 
 if __name__ == "__main__":
     main()
